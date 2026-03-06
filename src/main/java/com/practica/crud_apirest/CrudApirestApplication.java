667d3dd81fab4f45877b3939a74577885d43e959
Hugo <hugojariod77@gmail.com>
2026-03-06 14:27:15 +0100

    Inicializacion de repositorio; conexion con base de datos; parametrización de variables con dotenv

---
+++ src/main/java/com/practica/crud_apirest/CrudApirestApplication.java
@@ -0,0 +1,13 @@
+package com.practica.crud_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/crud_apirest/CrudApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.crud_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
